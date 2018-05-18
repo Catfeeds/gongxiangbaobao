7d0544d8f1485564b58b57dbd942cfa04c9c72fa
zhaogaoshang <zhaogaoshang@taihuoniao.com>
2018-05-18 18:58:47 +0800

    完成收获地址

---
+++ wx-LeXi/pages/orderInfo/orderInfo.js
@@ -0,0 +1,98 @@
+// pages/orderInfo/orderInfo.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    //添加到购物车的内容产品内容
+    shoppingCart: [
+      {
+        id: 6,
+        title: "图像加载被中断",
+        currentPrice: 500,
+        originPrice: 999,
+        logisticsExpenses: 0,//运费信息：0为没有运费用，包邮，其他为运费的价格
+        is_like: true,//是否喜欢
+        is_likeNumber: 66,//喜欢的人数
+        shopName: "bbq_BBQ_123亲",//店铺名称
+        shopingNumber: 1,//购买的数量
+        img: "http://www.hzxznjs.com/uploads/160728/1-160HQ64603a7.jpg",
+        color: "白色",
+        repertoryNumber: 12,
+        size: "M"
+      },
+    ],
+    order:[1,2]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+  
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+  
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+  
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+  
+  },
+  //选择其他物流
+  otherLogisticsTap(){
+    wx.navigateTo({
+      url: '../pickLogistics/pickLogistics',
+    })
+  },
+
+  //跳转到支付成功页面
+  paymentSuccess(){
+    wx.navigateTo({
+      url: '../paymentSuccess/paymentSuccess',
+    })
+  }
+
+})
---
+++ wx-LeXi/pages/receiveAddress/receiveAddress.js
@@ -0,0 +1,86 @@
+// pages/paymentAddress/paymentAddress.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    addres:[
+      {
+        name:"大白",
+        PNumber:13345670987,
+        addres:"贵州省遵义市红花岗区天通银丽水元2单元501",
+        addressInfo:"贵州省，遵义市",
+        addressNumber: 13381319070,
+        is_pick:false,
+        rid:1
+      },
+      {
+        name: "小黑",
+        PNumber: 18888888888,
+        addres: "贵州省遵义市红花岗区天通银丽水元2单元501",
+        addressInfo: "贵州省，遵义市",
+        addressNumber: 13381319070,
+        is_pick: false,
+        rid:2
+      },
+    ]
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+  
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+  
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+  
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+  
+  }
+})
