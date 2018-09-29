b67a7160325a7a7e9aa5b5ab2209ac1fdf60e18c
shawn <purpen.w@gmail.com>
2018-09-29 11:31:07 +0800

    update checkout

--- wx-LeXi/pages/checkout/checkout.js
+++ wx-LeXi/pages/checkout/checkout.js
@@ -437,12 +437,6 @@ Page({
 
   // 优惠券弹框里面的内容couponList
   couponTap(e) {
-<<<<<<< HEAD
-=======
-
-    console.log(e,"优惠券")
-    console.log(e.currentTarget.dataset.order_rid)
->>>>>>> origin/zhaogaoshang
     if (e.currentTarget.dataset.order_rid) {
       let coupon = this.data.couponList[e.currentTarget.dataset.order_rid]
       coupon.forEach((v, i) => {
